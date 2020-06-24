59b5dd87bd4a29bb57f0b7946d6a40e2f3dd12c5
Sean <sean.s.elias@outlook.com>
2020-06-24 20:18:08 +1000

    Added Action Filters

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { TimeagoModule } from 'ngx-timeago';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -59,6 +60,7 @@ export function tokenGetter() {
       ReactiveFormsModule,
       TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
+      TimeagoModule.forRoot(),
       JwtModule,
       NgxGalleryModule,
       FileUploadModule,
